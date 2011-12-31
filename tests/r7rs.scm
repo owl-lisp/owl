f27f9cc3da2c210e2032e656e7af84844ef7a593
Aki Helin <aki.helin@iki.fi>
2011-12-31 14:34:47 +0200

    added _ wildcard for macros (R7RS)

--- tests/r7rs.scm
+++ tests/r7rs.scm
@@ -9,6 +9,8 @@ Testing block comments. They could also be nested on second thought...
 
 |#
 
+;; test |symbol| syntax
+
 (let
    ((a '|foo bar|)
     (b (string->symbol "foo bar")))
@@ -18,7 +20,21 @@ Testing block comments. They could also be nested on second thought...
 (if (not (= 42 ((lambda (|foo|) foo) 42)))
    (print "symbolic failure 2"))
 
+;; returning both "" and "||" make sense. using the latter for now, but 
+;; might be that that behavior should only be done for write.
 (if (not (string=? "||" (symbol->string (string->symbol ""))))
    (print "symbolic failure 3"))
 
+;; test _ wildcard in macros
+(define-syntax foo
+   (syntax-rules (x)
+      ((_ x x) 2)
+      ((_ _ x) 1)
+      ((_ x _) 1)
+      ((_ . _) 0)))
+
+(if (not (equal? (list 2 1 1 0) (list (foo x x) (foo o x) (foo x o) (foo o o))))
+   (print (list (foo x x) (foo o x) (foo x o) (foo o o))))
+  
+;; shouldn't have printed anything before this
 (print "all done")
