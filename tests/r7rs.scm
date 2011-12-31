fff430a61ee5c2debe402aef3e0112b6eb6f00e5
Aki Helin <aki.helin@iki.fi>
2011-12-31 02:16:02 +0200

    added parts of alternative symbol syntax

--- tests/r7rs.scm
+++ tests/r7rs.scm
@@ -1,9 +1,21 @@
+(define (print arg)
+   (display arg)
+   (display "
+"))
+
 #|
 
 Testing block comments. They could also be nested on second thought...
 
 |#
 
+(let
+   ((a '|foo bar|)
+    (b (string->symbol "foo bar")))
+   (if (not (eq? a b))  
+      (print "symbolic failure 1")))
 
-(display "fini
-")
+(if (not (= 42 ((lambda (|foo|) foo) 42)))
+   (print "symbolic failure 2"))
+
+(print "all done")
