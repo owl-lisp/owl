b13ec7ef1d4f9f488eb5f7ab2c4d807769fbc866
Aki Helin <aki.helin@iki.fi>
2012-01-13 22:09:18 +0200

    added rename functionality to library exports, fixed a bug in library macro which dropped exports, and added rename library test

--- tests/library.scm
+++ tests/library.scm
@@ -17,3 +17,20 @@
       (define (baz x) (cons mine bar))
       (print (baz 42))))
 
+;; test renaming
+
+(define-library (rename)
+   (export 
+      (rename foo bar)
+      (rename bar foo))
+   (begin
+      (define foo "bar")
+      (define bar "foo")))
+
+(define-library (test)
+   (import (rename))
+   (export out)
+   (begin
+      (define out 42)
+      (print (list foo bar))))
+
