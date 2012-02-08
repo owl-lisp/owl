5e1206b0516e18b49653818df101319a7f24a5c5
Aki Helin <aki.helin@iki.fi>
2012-02-09 00:42:14 +0200

    hashbang test creates a script using given binary or vm+fasl and tries to run it

--- tests/hashbang.sh
+++ tests/hashbang.sh
@@ -1,3 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
-$@ tests/hashbang.scm 
+echo -n '#!'   > tmp/script
+for part in $@
+do
+   echo -n "`pwd`"
+   echo -n "/"
+   echo -n $part
+   echo -n " "
+done >> tmp/script
+
+echo "" >> tmp/script
+
+echo '
+
+(print "ohai")
+
+' >> tmp/script
+
+chmod +x tmp/script
+
+./tmp/script
