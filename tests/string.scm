f15124cf5ab21a290dadadbdc607ebda6bdc28db
Aki Helin <aki.helin@iki.fi>
2012-02-11 19:18:07 +0200

    flushing stdio output ports at exit and fixed a broken string comparison test

--- tests/string.scm
+++ tests/string.scm
@@ -1,6 +1,10 @@
-(define (check val wanted)
-   (if (not (eq? val wanted))
-      (display "boo!")))
+(define-syntax check
+   (syntax-rules ()
+      ((check (op a b) res)
+         (if (not (eq? res (op a b)))
+            (begin 
+               (write (list op a b))
+               (print "FAIL"))))))
 
 (check (string<? "" "")      #false)
 (check (string<? "" "a")     #true)
@@ -8,7 +12,7 @@
 (check (string<? "a" "a")    #false)
 (check (string<? "aa" "ab")  #true)
 
-(check (string<=? "" "")     #false)
+(check (string<=? "" "")     #true)
 (check (string<=? "" "a")    #true)
 (check (string<=? "a" "")    #false)
 (check (string<=? "a" "a")   #true)
