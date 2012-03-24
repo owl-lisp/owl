2052c8a932e190f299ba3fe6e172d9c72db6d8f3
Aki Helin <aki.helin@iki.fi>
2012-03-24 11:05:32 +0200

    fixed bailing via two correct results in last commit

--- tests/errmsg-e.sh
+++ tests/errmsg-e.sh
@@ -4,6 +4,6 @@ STDERR="tmp/errmsg-e2-$$"
 STDOUT="tmp/errmsg-e1-$$"
 $@ -e '(list 1 2 3 fiddlesticks)' 1>$STDOUT 2>$STDERR || echo 'got nonzero exit'
 grep -q fiddlesticks $STDERR && echo 'found sticks' || exit 1
-test -s $STDOUT || echo 'stdout is blank' && exit 2
+test -s $STDOUT || { echo 'stdout is blank' || exit 2; }
 rm $STDERR $STDOUT
 
