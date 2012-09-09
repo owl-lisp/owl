cbdc3dfbbe3d454bf5bdcb0e930e336361deb9d1
Aki Helin <aki.helin@iki.fi>
2012-09-09 14:50:21 +0300

    redirecting cat stderr away in case it complains about broken pipe in vector file io test as per Juha Mustonen's suggestion.

--- tests/vecio.sh
+++ tests/vecio.sh
@@ -2,7 +2,7 @@
 
 FILE="tmp/test-vecio-orig-$$.scm"
 COPY="tmp/test-vecio-copy-$$.scm"
-cat /dev/urandom | head -n 1000 > $FILE
+cat /dev/urandom 2>/dev/null | head -n 1000 > $FILE
 $@ -e "(vector->file (file->vector \"$FILE\") \"$COPY\") \"done\""
 diff $FILE $COPY
 rm $FILE $COPY
