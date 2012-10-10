9f9f761ac74d6fc2d8493cec330dbb83427f73f0
Aki Helin <aki@haltp.org>
2012-10-11 00:16:16 +0300

    tried to finish unbalanced transitional ff-ng trees, ended up starting experiment how horrible code would become if one enforced a stricter balancing invariant than red-black trees, namely that every node is off-balance by at most one node. this will likely seem like a bad idea after captain morgan wears out.

--- owl/defmac.scm
+++ owl/defmac.scm
@@ -34,8 +34,16 @@
       type-port 
       type-socket
       type-tcp-client
-      type-ff               ;; k v, k v l or i v l r
-      type-ff-right         ;; k v r
+
+      ;; sketching types
+      type-ff               ;; k v, k v l r
+      type-ff-right         ;; k v r, k v l r+
+      type-ff-left          ;; k v l, k v l+ r
+
+      ;; k v, l k v r       -- type-ff
+      ;; k v r, k v l r+    -- type-ff-right
+      ;; k v l, k v l+ r    -- type-ff-leftc
+
    )
 
    (begin
@@ -482,9 +490,11 @@
       (define type-rlist-node       14)
       (define type-rlist-spine      10)
 
-      ;; transitional trees
-      (define type-ff               24)   ;; black[key val] or black[key val left] or black[key val left right]
-      (define type-ff-right         25)   ;; black[key val right]
+      ;; transitional trees or future ffs
+      (define type-ff               24)
+      (define type-ff-right         25)
+      (define type-ff-left          26)
+
       ; + type-ff-red, type-ff-right
 
       ; 8 - black ff leaf
