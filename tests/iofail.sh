af6bd9fb64cfe7db628087befd07f6087bba22f9
Aki Helin <aki@haltp.org>
2012-11-28 10:59:01 +0200

    made test relying on /dev/full optional, since it seems to be missing at least on osx

--- tests/iofail.sh
+++ tests/iofail.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+test -f /dev/full || exit 0 # doesn't exist on some systems
+
 # output errors in typical shell script use are errors 
-$@ -e '(+ 40 2)' >/dev/full || echo ok 1
+$@ -e '(+ 40 2)' >/dev/full && echo bad 1
 
 # output errors in interactive mode are fatal
-$@ >/dev/full || echo ok 2
+$@ >/dev/full && echo bad 2
 
