478fabd1e47c34c7a3c69d32725b508161be4961
Aki Helin <aki.helin@iki.fi>
2015-09-19 19:29:05 +0300

    not leaving a trailing whitespace in hashbang test (causes test to fail in openbsd)

--- tests/hashbang.sh
+++ tests/hashbang.sh
@@ -3,7 +3,7 @@
 ME=$$
 HERE=$(pwd)
 
-echo '(lambda (args) (print (foldr string-append "" (list "#!" (foldr (lambda (a b) (string-append a (string-append " " b))) "" (map (lambda (x) (string-append ' "\"$HERE/\"" ' x)) (cdr args))) "\n" "(print \"ohai\")"))))' | $@ --run - $@ > tmp/script-$ME
+echo '(lambda (args) (print (foldr string-append "" (list "#!" (foldr (lambda (a b) (if (equal? b "") a (string-append a (string-append " " b)))) "" (map (lambda (x) (string-append ' "\"$HERE/\"" ' x)) (cdr args))) "\n" "(print \"ohai\")"))))' | $@ --run - $@ > tmp/script-$ME
 
 chmod +x tmp/script-$ME
 
@@ -13,4 +13,3 @@ test $(head -n 1 ./tmp/script-$ME | wc -c) -gt 127 && { echo ohai; exit 0; }
 ./tmp/script-$ME | grep "^ohai$" || exit 1
 
 rm tmp/script-$ME
-
