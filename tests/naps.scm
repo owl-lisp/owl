a97c418a217b1591d8a9efe247e64f6a2555314a
Aki Helin <aki.helin@solita.fi>
2015-12-13 20:16:46 +0200

    allowing occasional gc pause in naps test

--- tests/naps.scm
+++ tests/naps.scm
@@ -6,7 +6,7 @@
         (< got (+ want max-ms-oversleep))))
 
 (define (random-napper seed n)
-   (let loop ((rs (seed->rands seed)) (n n))
+   (let loop ((rs (seed->rands seed)) (n n) (failed? #false))
       (set-ticker 0)
       (if (> n 0)
          (lets 
@@ -14,9 +14,17 @@
              (start (time-ms))
              (_ (sleep x))
              (elapsed (- (time-ms) start)))
-            (if (close-enough? x elapsed)
-               (loop rs (- n 1))
-               (print "Tried to sleep " x " but slept " elapsed "!")))
+            (cond
+               ((close-enough? x elapsed)
+                  (loop rs (- n 1) #false))
+               (failed?
+                  (print "Tried to sleep " x " but slept " elapsed "!"))
+               ((< elapsed x)
+                  (print "Tried to sleep " x " but slept " elapsed "!"))
+               (else
+                  ;; possible to oversleep on occasion, if a big gc happens
+                  ;; in the meantime
+                  (loop rs (- n 1) #true))))
          (print "ok"))))
 
 (begin
