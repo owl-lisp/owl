fd329ab7e30340a48cc962b313ccdfc8c0ed34f7
Aki Helin <aki.helin@solita.fi>
2015-12-25 21:05:28 +0200

    added some content quotation

--- owl/html.scm
+++ owl/html.scm
@@ -1,12 +1,8 @@
 (define-library (owl html)
 
-   (export
-      html
-      ; html->sexp
-      )
+   (export html)
 
-   (import
-      (owl base))
+   (import (owl base))
 
    (begin
 
@@ -20,6 +16,23 @@
             (else
                (ret #false))))
 
+      ;; quote \:s and ":s with \. e.g. a""b → a\"\"b → a\\\"\\\"b → ...
+
+      (define (quote-once lst tail)
+         (foldr
+            (λ (char tail)
+               (if (or (eq? char #\") (eq? char #\\))
+                  (ilist #\\ char tail)
+                  (cons char tail)))
+            tail lst))
+
+      (define (render-quoted val tail)
+         (cond
+            ((string? val)
+               (let ((cs (string->list val)))
+                  (quote-once cs tail)))
+            (else
+               (error "render-quoted: cannot handle this yet: " val))))
 
       (define (gen-attrs pairs tail)
          (if (null? pairs)
@@ -29,7 +42,7 @@
                (cons #\space 
                   (render (car fst)
                      (list* #\= #\"
-                        (render (cadr fst)
+                        (render-quoted (cadr fst)
                            (cons #\" (gen-attrs (cdr pairs) tail)))))))))
 
       (define open-only?
@@ -44,6 +57,15 @@
              (ff (list->ff (zip cons keys keys))))
             (λ (x) (get ff x #false))))
 
+      (define (render-quoting-tags str tail)
+         (str-foldr
+            (λ (c tail)
+               (cond
+                  ((eq? c #\<) (ilist #\& #\l #\t #\; tail))
+                  ((eq? c #\>) (ilist #\& #\g #\t #\; tail))
+                  (else (cons c tail))))
+            tail str))
+
       (define (generate ret)
 
          (define (maybe-gen-attrs lst tail)
@@ -51,33 +73,30 @@
                ((null? lst) tail)
                ((and (pair? (car lst)) (pair? (caar lst)))
                   (gen-attrs (car lst)
-                     (cons #\>
-                        (foldr gen tail (cdr lst)))))
+                     (cons #\> (foldr gen tail (cdr lst)))))
                (else
                   (cons #\> 
                      (foldr gen tail lst)))))
 
          (define (gen exp tail)
             (cond
-               ((null? exp)
-                  tail)
-               ((not exp)
-                  tail)
+               ((null? exp) tail)
+               ((not exp) tail)
                ((pair? exp)
                   (let ((hd (car exp)))
-                     (cond
-                        ((open-only? hd)
-                           (cons #\< 
-                              (render hd
-                                 (maybe-gen-attrs (cdr exp) tail))))
-                        ((closed-tag? hd)
-                           (cons #\< 
+                     (cons #\<
+                        (cond
+                           ((open-only? hd)
+                              (render hd (maybe-gen-attrs (cdr exp) tail)))
+                           ((closed-tag? hd)
                               (render hd 
                                  (maybe-gen-attrs (cdr exp)
                                     (list* #\< #\/ 
-                                       (render hd (cons #\> tail)))))))
-                        (else
-                           (ret #false)))))
+                                       (render hd (cons #\> tail))))))
+                           (else
+                              (ret #false))))))
+               ((string? exp)
+                  (render-quoting-tags exp tail))
                (else
                   (render exp tail))))
 
@@ -101,11 +120,7 @@
          (ul
             (li (a ((href "http://lol") (target "_top")) "trololo"))
             (li (p "foo" (b "bar") "baz"))
+            (li ((style "foo: \"lol\"")) "ke<script>alert(42);</script>kekek")
             (li (p "BAR"))))))
-
-
-;; how about attributes?
-;; (a (foo . bar) ...)?
-;; (a (href . "foo") ..)
 
       
