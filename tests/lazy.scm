798bf96eb0e48982dbe8fe7ab2e5e227fce8fd02
Aki Helin <aki.helin@iki.fi>
2016-02-27 11:08:19 +0100

    started adding laziness tracking to tests

--- tests/lazy.scm
+++ tests/lazy.scm
@@ -7,3 +7,27 @@
       (else (list x x))))  ;; double big odds
 
 (print (force-ll (ltake (ledit editor (lnums 1)) 20)))
+
+
+;; start checking laziness/eagerness preservation 
+
+(define (boom . tail)
+   (lambda ()
+      (print "BOOM! tail got computed")
+      tail))
+
+(define three-elems (ilist 0 1 2 (boom 3 4 5)))
+
+(define (computed x)
+   (cond
+      ((pair? x) (cons (car x) (computed (cdr x))))
+      ((null? x) x)
+      ((function? x) 'TBC)
+      (else 'ERROR)))
+
+(define print-computed 
+   (o print computed))
+
+;; 
+
+(print-computed three-elems)
