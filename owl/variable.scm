1a9718e05893c9dc79c7ab0196f6460f6ffe8b09
Aki Helin <aki.helin@iki.fi>
2018-05-25 23:00:00 +0300

    added thread-local variable bindings

--- owl/variable.scm
+++ owl/variable.scm
@@ -10,38 +10,64 @@
 
    (import
       (owl defmac)
-      (only (owl syscall) mail interact wait-mail thread thunk->thread)
+      (owl tuple)
+      (owl list)
+      (owl io)
+      (only (owl syscall) mail interact wait-mail thread thunk->thread link)
       (owl ff))
 
    (begin
 
-      ;; unique value
+      ;; unique values
       (define read-msg (list 1))
+      (define read-local-msg (list 2))
 
       (define (handler id)
          (case-lambda
             (() (interact id read-msg))          ;; read (sync)
             ((val) (mail id (cons 'set val)))    ;; write (async)
-            ((op val) (mail id (cons op val))))) ;; write (async)
+            ((op val) (interact id (cons op val))))) ;; any (sync)
 
-      (define (store val)
+      (define (store val env)
          (lets ((envelope (wait-mail))
                 (from msg envelope))
-            (if (eq? msg read-msg)
-               (begin
+            (cond
+               ((eq? msg read-msg)
                   (mail from val)
-                  (store val))
-               (let ((op (car msg)))
-                  (cond
-                     ((eq? op 'set)
-                        (store (cdr msg)))
-                     ((eq? op 'call)
-                        (store ((cdr msg) val)))
-                     (else
-                        (store val)))))))
+                  (store val env))
+               ((eq? msg read-local-msg)
+                  (mail from val)
+                  (store val env))
+               ((pair? msg)
+                  (let ((op (car msg)))
+                     (cond
+                        ((eq? op 'set)
+                           (mail from (cdr msg))
+                           (store (cdr msg) env))
+                        ((eq? op 'call)
+                           (let ((res ((cdr msg) val)))
+                              (mail from res)
+                              (store res env)))
+                        ((eq? op 'get-local)
+                           (mail from (get env from (cdr msg)))
+                           (store val env))
+                        ((eq? op 'set-local)
+                           (mail from (cdr msg))
+                           (let ((lenv (get env from #f)))
+                              (if lenv
+                                 (store val
+                                    (fupd env from (cdr msg)))
+                                 (begin
+                                    (link from) ;; we want to know when it exists
+                                    (store val (put env from (cdr msg)))))))
+                        (else
+                           (store val env)))))
+               (else
+                  ;; a linked thread has finished - drop local env
+                  (store val (del env from))))))
 
       (define (start-variable id val)
-         (thread id (store val))
+         (thread id (store val #empty))
          (handler id))
 
       (define make-variable
---
+++ tests/thread-variable.scm
@@ -0,0 +1,16 @@
+(import
+  (owl variable))
+
+(define foo (make-variable))
+
+(foo 'set 11)
+(print (foo) " = " 11)
+
+(begin
+   (foo 'set-local 22)
+   (print (foo 'get-local 'missing) " = " 22) ;; this runs in the same thread
+   (print (foo) " = " 11)) ;; global is the same
+
+(print (foo) " = 11") ;; global is the same here also
+
+(print (foo 'get-local 404) " = " 404) ;; this runs in another thread, previous bindings are no longer there
